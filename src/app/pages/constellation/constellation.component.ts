c9b681dfa38e2e140b4a9f9f524cb1c9a70e21f7
Patrick McLean <lavillelumiere8@gmail.com>
2023-06-15 16:36:09 -0400

    skeleton

--- src/app/pages/constellation/constellation.component.ts
+++ src/app/pages/constellation/constellation.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-constellation',
   templateUrl: './constellation.component.html',
   styleUrls: ['./constellation.component.css']
 })
-export class ConstellationComponent {
+export class ConstellationComponent implements OnInit {
 
+  constellationForm!: FormGroup;
+
+  constellations = [
+    { id: 1, name: "Ares" },
+    { id: 2, name: "Taurus" },
+    { id: 3, name: "Gemini" },
+    { id: 4, name: "Cancer" },
+    { id: 5, name: "Leo" },
+    { id: 6, name: "Virgo" },
+    { id: 7, name: "Libra" },
+    { id: 8, name: "Scorpio" },
+    { id: 9, name: "Sagittarius" },
+    { id: 10, name: "Capricorn" },
+    { id: 11, name: "Aquarius" },
+    { id: 12, name: "Pisces" }
+  ];
+
+  constructor(private fb:FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.constellationForm = this.fb.group({
+    constellation: [null]
+    });
+  }
+
+  submit() {
+    console.log("Form submitted")
+    console.log(this.constellationForm.value);
+  }
 }
--- src/app/pages/geolocation/geolocation.component.ts
+++ src/app/pages/geolocation/geolocation.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-geolocation',
   templateUrl: './geolocation.component.html',
   styleUrls: ['./geolocation.component.css']
 })
-export class GeolocationComponent {
+export class GeolocationComponent  implements OnInit {
+
+    geolocationForm!: FormGroup;
+
+    geolocations = [
+      { id: 1, name: "Ares" },
+      { id: 2, name: "Taurus" },
+      { id: 3, name: "Gemini" },
+      { id: 4, name: "Cancer" },
+      { id: 5, name: "Leo" },
+      { id: 6, name: "Virgo" },
+      { id: 7, name: "Libra" },
+      { id: 8, name: "Scorpio" },
+      { id: 9, name: "Sagittarius" },
+      { id: 10, name: "Capricorn" },
+      { id: 11, name: "Aquarius" },
+      { id: 12, name: "Pisces" }
+    ];
+
+    constructor(private fb:FormBuilder) {
+    }
+
+    ngOnInit() {
+      this.geolocationForm = this.fb.group({
+      geolocation: [null]
+      });
+    }
+
+    submit() {
+      console.log("Form submitted")
+      console.log(this.geolocationForm.value);
+    }
 
 }
